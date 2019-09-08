894a013e46f420d024fa75f7d368e4911f839bd7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 16:06:15 -0400

    Add class constructor examples

---
+++ Classes/ClassConstructors/ClassConstructors/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassConstructors
+{
+    public class Order
+    {
+
+    }
+}
---
+++ Classes/ClassConstructors/ClassConstructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Ryan";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+        }
+    }
+}
