69ebcc2a8979e726d19085eeb0e2c17fe4facf20
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 19:09:14 -0400

    Add Stopwatch exercise

---
+++ Classes/StopwatchExercise/StopwatchExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopwatchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            Console.WriteLine("Press ENTER to start timer and ENTER to stop timer.");
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    stopWatch.StartTimer();
+                    Console.ReadLine();
+                    stopWatch.StopTimer();
+                    Console.WriteLine(stopWatch.Duration().TotalSeconds + " seconds elapsed.");
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
---
+++ Classes/StopwatchExercise/StopwatchExercise/Stopwatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StopwatchExercise
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime = DateTime.UtcNow;
+        private DateTime _endTime;
+        private bool _previouslyStarted;
+
+        public void StartTimer()
+        {
+            // Start timer if it hasn't already been started.
+            if (_previouslyStarted)
+            {
+                throw new InvalidOperationException("Timer has already been started");
+            }
+
+            Console.WriteLine("Timer started.");
+
+            _startTime = DateTime.UtcNow;
+            _previouslyStarted = true;
+        }
+
+        public void StopTimer()
+        {
+            this._endTime = DateTime.UtcNow;
+            _previouslyStarted = false;
+            Console.WriteLine("Timer stopped.");
+            
+        }
+
+        public TimeSpan Duration()
+        {
+            // Returns the total time elapsed.
+            return _endTime - _startTime;
+        }
+    }
+}
