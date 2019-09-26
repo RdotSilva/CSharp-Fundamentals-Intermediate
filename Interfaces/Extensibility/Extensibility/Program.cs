131746875b736eee6a5533aa21c771b9bfc8d50b
Ryan Silva <RyandotSilva@gmail.com>
2019-09-26 19:56:32 -0400

    Add extensibility example

---
+++ Interfaces/Extensibility/Extensibility/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\test\\test2.txt"));
+            dbMigrator.Migrate();
+            
+        }
+    }
+}
