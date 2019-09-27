443157c78e06691cf4f36acbf25a5b043f5fe1d7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-26 21:06:03 -0400

    Add Stack exercise

---
+++ Inheritance/Stack/Stack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push("Cool");
+            stack.Push(5);
+            stack.Push("Ok");
+            stack.Clear();
+            stack.Push(true);
+            stack.Push(2525);
+
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Inheritance/Stack/Stack/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private readonly List<object> _stackList = new List<object>();
+
+        public void Push(object item)
+        {
+            if (item == null)
+                throw new InvalidOperationException("Item can't be null.");
+
+            _stackList.Add(item);
+            
+        }
+
+        public object Pop()
+        {
+            if (_stackList.Count == 0)
+                throw new InvalidOperationException("No elements in stack.");
+
+            var index = _stackList.Count - 1;
+
+            var poppedItem = _stackList[index];
+
+            _stackList.RemoveAt(index);
+
+            return (poppedItem);
+        }
+
+        public void Clear()
+        {
+            _stackList.Clear();
+        }
+    }
+}
